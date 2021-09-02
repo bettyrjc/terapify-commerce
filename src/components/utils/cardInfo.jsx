c9db12a23d378ee2378f4184c45654753ac3022e
Betty Jimenez <bettyjimenez3010@gmail.com>
2021-09-01 22:10:02 -0400

    :art: add: card hover, it is static added

---
+++ src/components/utils/cardInfo.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { descriptionCard } from "Src/store/mock";
+const cardInfo = () => {
+  console.log("descriptionCard", descriptionCard);
+  return (
+    <div className="mx-10">
+      <h2
+        style={{ fontSize: "3rem" }}
+        className="text-green-200 mt-10 border-b-2 border-green-200 mx-8 pb-6 font-extrabold ml-6 text-center my-10"
+      >
+        Be a fiel client
+      </h2>
+      <div className="grid grid-cols-3 gap-3">
+        {descriptionCard.map((data) => {
+          return (
+            <div
+              key={data.id}
+              className="sm:mt-2 md:mt-0 w-full h-auto bg-white text-gray-500 hover:text-green-200 hover:shadow-2xl p-6 rounded-lg flex flex-col justify-between "
+            >
+              <div className="flex item-center justify-center w-full">
+                {data.icon}
+              </div>
+              <h2 className="text-2xl my-2 text-center">{data.title}</h2>
+              <p className="text-gray-700">{data.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default cardInfo;
---
+++ src/store/mock.js
@@ -0,0 +1,32 @@
+import {
+  IoAccessibility,
+  IoCashOutline,
+  IoConstructSharp,
+} from "react-icons/io5";
+
+export const descriptionCard = [
+  {
+    id: 1,
+    order: 1,
+    icon: <IoCashOutline style={{ fontSize: "6rem" }} />,
+    title: "application online",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur mollitia incidunt culpa. Magnam dolorum ea sit vero. accusamus dolor sint.",
+  },
+  {
+    id: 2,
+    order: 2,
+    icon: <IoConstructSharp style={{ fontSize: "6rem" }} />,
+    title: "get an approval",
+    description:
+      "Lorem ipsum dolor sit amet conspa. Magnam  accusamus dolor sint.",
+  },
+  {
+    id: 3,
+    order: 3,
+    icon: <IoAccessibility style={{ fontSize: "6rem" }} />,
+    title: "card delivery",
+    description:
+      "Lorem psum dolor sit amet c psum dolor sit amet cipsum dolor sit amet conspa. Magnam  accusamus dolor sint.",
+  },
+];
