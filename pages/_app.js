00aa127ff2ff66d6fcda1d8429b5d989106b1a90
Betty Jimenez <bettyjimenez3010@gmail.com>
2021-09-05 12:07:28 -0400

    :sparkles: add: i18n config in next

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,27 @@ import { Provider } from "react-redux";
 import Head from "next/head";
 import store from "../src/store/reducers";
 import { ToastContainer } from "react-toastify";
+import useTranslation from "next-translate/useTranslation";
 
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/tailwind.scss";
 
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
+
 function MyApp({ Component, pageProps }) {
+  let { t } = useTranslation();
+  const title = t("common:title");
   return (
     <Provider store={store}>
       <Head>
-        <title>PRODUCTOS</title>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content="Add a shopping cart to your site in minutes. Works with any site builder, CMS, and framework. 20 000+ merchants trust our e-commerce solution for their website. Join them!"
+        ></meta>
       </Head>
       <ToastContainer
         position="top-right"
@@ -23,6 +35,7 @@ function MyApp({ Component, pageProps }) {
         draggable
         pauseOnHover
       />
+  
       <Component {...pageProps} />
     </Provider>
   );
